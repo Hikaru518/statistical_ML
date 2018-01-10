f09cf61f06ec1ddcbc8c496bf1b428650ae3c0c4
Wang PG <WPG@bupt.edu.cn>
2018-01-10 16:47:21 -0600

    Add two Categorical and Normal distribution

--- proj1/sampler.py
+++ proj1/sampler.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 class ProbabilityModel:
 
     # Returns a single sample (independent of values returned on previous calls).
@@ -14,8 +17,21 @@ class UnivariateNormal(ProbabilityModel):
     # Initializes a univariate normal probability model object
     # parameterized by mu and (a positive) sigma
     def __init__(self,mu,sigma):
-        pass
-    
+        self.Mu=Mu
+        self.Sigma=Sigma
+
+    def sample(self):
+    	sampleRes = 0
+    	Num_UnifromD = 25
+    	# Use the sum of 25 i.i.d uniform distribution to approximate the normal distribution 
+    	for i in xrange(Num_UnifromD): 
+    		randNum = np.random.uniform(-np.sqrt(3),np.sqrt(3))
+    		sampleRes = sampleRes + randNum
+    	sampleRes = np.sqrt(Num_UnifromD)*sampleRes
+    	sampleRes = sampleRes*self.sigma + self.Mu
+    	return sampleRes
+
+
 # The sample space of this probability model is the set of D dimensional real
 # column vectors (modeled as numpy.array of size D x 1), and the probability 
 # measure is defined by the density function 
@@ -26,8 +42,13 @@ class MultiVariateNormal(ProbabilityModel):
     # parameterized by Mu (numpy.array of size D x 1) expectation vector 
     # and symmetric positive definite covariance Sigma (numpy.array of size D x D)
     def __init__(self,Mu,Sigma):
-        pass
-    
+        self.Mu=Mu
+        self.Sigma=Sigma
+
+    def sample(self):
+    	pass
+
+
 
 # The sample space of this probability model is the finite discrete set {0..k-1}, and 
 # the probability measure is defined by the atomic probabilities 
@@ -38,9 +59,17 @@ class Categorical(ProbabilityModel):
     # probability model object with distribution parameterized by the atomic probabilities vector
     # ap (numpy.array of size k).
     def __init__(self,ap):
-        pass
+        self.ap=ap
 
+    def sample(self):
+    	N = len(self.ap)
+    	Cumulative_ap = [sum(self.ap[:i]) for i in range(1,N+1)]
 
+    	randNum = np.random.uniform(0,1)
+    	for i in xrange(N):
+    		if (randNum < Cumulative_ap[i]):
+    			return i
+    
 # The sample space of this probability model is the union of the sample spaces of 
 # the underlying probability models, and the probability measure is defined by 
 # the atomic probability vector and the densities of the supplied probability models
@@ -52,3 +81,4 @@ class MixtureModel(ProbabilityModel):
     # probability models pm
     def __init__(self,ap,pm):
         pass
+
