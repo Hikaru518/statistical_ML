c816f19f5f7445129d3511ab920ced34416e52b1
Wang PG <WPG@bupt.edu.cn>
2018-03-04 15:21:46 -0600

    add multiclass SVM

--- proj4/linear_svm.py
+++ proj4/linear_svm.py
@@ -51,7 +51,7 @@ def binary_svm_loss(theta, X, y, C):
 # SVM multiclass
 
 def svm_loss_naive(theta, X, y, reg):
-  """
+    """
   Structured SVM loss function, naive implementation (with loops).
 
   Inputs have dimension d, there are K classes, and we operate on minibatches
@@ -67,14 +67,14 @@ def svm_loss_naive(theta, X, y, reg):
   Returns a tuple of:
   - loss J as single float
   - gradient with respect to weights theta; an array of same shape as theta
-  """
+    """
 
-  K = theta.shape[1] # number of classes
-  m = X.shape[0]     # number of examples
+    K = theta.shape[1] # number of classes
+    m = X.shape[0]     # number of examples
 
-  J = 0.0
-  dtheta = np.zeros(theta.shape) # initialize the gradient as zero
-  delta = 1.0
+    J = 0.0
+    dtheta = np.zeros(theta.shape) # initialize the gradient as zero
+    delta = 1.0
 
   #############################################################################
   # TODO:                                                                     #
@@ -83,9 +83,22 @@ def svm_loss_naive(theta, X, y, reg):
   # code above to compute the gradient.                                       #
   # 8-10 lines of code expected                                               #
   #############################################################################
-
-
-
+    
+    L = []
+    for i in range(m):
+        tempSum = 0
+        tmp = 0
+        for j in range(K):
+            if(j != y[i]):
+                tmp = max(0,np.dot(theta[:,j].T,X[i]) - np.dot(theta[:,y[i]].T,X[i]) + delta)
+                tempSum += tmp
+                if(tmp>0):
+                    dtheta[:,j] += X[i]
+                    dtheta[:,y[i]] -= X[i]
+        L.append(tempSum)
+        
+    J = np.mean(L) + 0.5*reg*np.linalg.norm(theta)*np.linalg.norm(theta)
+    
   #############################################################################
   # TODO:                                                                     #
   # Compute the gradient of the loss function and store it dtheta.            #
@@ -95,19 +108,20 @@ def svm_loss_naive(theta, X, y, reg):
   # code above to compute the gradient.                                       #
   #############################################################################
 
-
-  return J, dtheta
-
+    dtheta = 1.0*dtheta/m + reg*theta
+                       
+    return J, dtheta
 
 def svm_loss_vectorized(theta, X, y, reg):
-  """
+    """
   Structured SVM loss function, vectorized implementation.
 
   Inputs and outputs are the same as svm_loss_naive.
-  """
-  J = 0.0
-  dtheta = np.zeros(theta.shape) # initialize the gradient as zero
-  delta = 1.0
+    """
+
+    J = 0.0
+    dtheta = np.zeros(theta.shape) # initialize the gradient as zero
+    delta = 1.0
 
   #############################################################################
   # TODO:                                                                     #
@@ -116,6 +130,17 @@ def svm_loss_vectorized(theta, X, y, reg):
   # 8-10 lines of code                                                        #
   #############################################################################
   
+    m = X.shape[0] 
+
+    scores = np.dot(X,theta)
+    correct_class_scores = scores[np.arange(m), y]   # 1 by N
+    correct_class_scores = np.reshape(correct_class_scores, (m, -1))  # N by 1
+    margin=scores-correct_class_scores + delta 
+    margin[np.arange(m),y]=0.0 
+    margin[margin<=0]=0.0
+    J+=np.sum(margin)
+    J = J/m + 0.5*reg * np.sum(theta * theta) 
+    
 
 
   #############################################################################
@@ -132,9 +157,15 @@ def svm_loss_vectorized(theta, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-
+    
+    margin[margin>0]=1.0
+    row_sum = np.sum(margin,axis=1)
+    margin[np.arange(m),y] = -row_sum
+    dtheta = 1.0/m*np.dot(X.T,margin) + reg*theta;
+    
+    
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
 
-  return J, dtheta
+    return J, dtheta
